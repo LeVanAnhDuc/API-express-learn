dfbf5afaadc8d8597167f12cf50b1489bd4ab5db
LrVanAnhDuc <levananhduc1804@gmail.com>
2024-04-22 23:29:09 +0700

    refactor (nodemon): config webpack

--- src/utils/hashing.util.ts
+++ src/utils/hashing.util.ts
@@ -3,9 +3,9 @@ import * as bcrypt from 'bcrypt';
 const saltOrRounds = 10;
 
 export const hashPassword = async (password: string) => {
-  return await bcrypt.hash(password, saltOrRounds);
+    return await bcrypt.hash(password, saltOrRounds);
 };
 
 export const isValidPassword = async (password: string, hash: string) => {
-  return await bcrypt.compare(password, hash);
+    return await bcrypt.compare(password, hash);
 };
